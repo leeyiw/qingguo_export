ccf8d635396b745f3198c03ad4c8f344e17ecfd8
伯成 <yiwei.liyw@alibaba-inc.com>
2016-07-17 23:39:45 +0800

    config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@
 # -*- coding: utf-8 -*-
 
 DOWNLOAD_DIR = '/Users/liyiwei/qingguo_videos'
-SESSION_ID = 'your session ID here'
+SESSION_ID = 'your smartcamera_buy_pc_fg_sessionid here'
 DEVICE_ID = 'your device ID here'
